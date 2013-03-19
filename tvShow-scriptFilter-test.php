405a28c4955b794b45364d950dca7093ebf144fc
Kevin Kirsche <kkirsche@hsc.edu>
2013-03-19 15:38:51 -0400

    Updated to version 0.11 — Season(s) name fix.

--- tvShow-scriptFilter-test.php
+++ tvShow-scriptFilter-test.php
@@ -1,4 +1,9 @@
 <?php
+     
+     // Author: Kevin Kirsche
+     // Version: 0.11
+
+     date_default_timezone_set('America/New_York');
 
 	require_once('workflows.php');
 	require_once('TVRAGE/TVRAGE.class.php');
@@ -8,7 +13,7 @@
 	//create new workflow
 	$w = new Workflows();
 	// Grab input
-	$input = "Mike & Molly";
+	$input = "{query}";
 
 	//Use the input to search TVRage
 	$show = TV_Shows::search($input);
@@ -27,11 +32,18 @@
      		'seasons' => $each->seasons,
      	);
 
+          //check number of seasons to ensure we return the correct word
+          if ($thisShow['seasons'] > 1) {
+               $seasons = "seasons";
+          } else {
+               $seasons = "season";
+          }
+
      	//set the subtitle based on the status of the show. We want the user to get information pertinent to the show.
      	if($thisShow['status'] == "Returning Series") {
      		$thisShow['subtitle'] = $each->name . " is a " . $thisShow['status'] . ". It is aired " . $thisShow['airDay'] . " at " . $thisShow['airTime'] . " on " . $thisShow['network'] . ".";
      	} else if ($thisShow['status'] == "Canceled/Ended") {
-     		$thisShow['subtitle'] = $thisShow['name'] . " is " . $thisShow['status'] . ". It ran for " . $thisShow['seasons'] . " seasons on " . $thisShow['network'] . ".";
+     		$thisShow['subtitle'] = $thisShow['name'] . " is " . $thisShow['status'] . ". It ran for " . $thisShow['seasons'] . " $seasons on " . $thisShow['network'] . ".";
      	} else if ($thisShow['status'] == "TBD/On The Bubble") {
      		$thisShow['subtitle'] = $thisShow['name'] . " is on the bubble for being renewed. If it is renewed, it airs on " . $thisShow['airDay'] . " at " . $thisShow['airTime'] . " on " . $thisShow['network'] . ".";
      	} else {
